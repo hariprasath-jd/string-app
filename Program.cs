a100281a5022229711672ac3db4d61a51d02ada6
Hari Prasath S <hprasath21212@gmail.com>
2023-12-06 16:47:34 +0530

    Loaded System.commandLine

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.CommandLine;
+
+namespace App.StringOperation {
+    class MainProgram
+    {
+        public static void Main(string[] args)
+        {
+            var printOption = new Option<string>(new string[] {"-p","--print"},"Print the given value.");
+
+            var c = new RootCommand();
+            c.AddOption(printOption);
+
+            c.SetHandler(pr => {
+                var output = pr.ParseResult.GetValueForOption(printOption);
+                Console.WriteLine($"{output}");
+            });
+            c.Invoke(args);
+        }
+    }
+}
